b1c1d379d594586569ca93e2f2c5a214c68f103b
jarvis <jsd.ditcher@outlook.com>
2024-04-16 23:37:13 +0100

    add basic html for edit,view,create note

--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
-    <h1>Create!</h1>
+    <h1>Create a note!</h1>
+    <div class="note-container single-note">
+        <form action="{{ route('note.store') }}" method="post" class="note">
+            <textarea name="note" cols="24" rows="10" class="note-body" placeholder="Enter note content here"></textarea>
+            <div class="note-buttons">
+                <a href="{{ route('note.index') }}" class="note-cancel-button">Cancel</a>
+                <button class="note-submit-button">Submit</button>
+            </div>
+        </form>
+    </div>
 </x-layout>
--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
     <h1>Edit!</h1>
+    <div class="note-container single-note">
+        <form action="{{ route('note.update', $note) }}" method="post" class="note">
+            <textarea name="note" cols="24" rows="10" class="note-body" placeholder="Update note here">
+                {{ $note->note }}
+            </textarea>
+            <div class="note-buttons">
+                <a href="{{ route('note.index') }}" class="note-cancel-button">Cancel</a>
+                <button class="note-submit-button">Submit</button>
+            </div>
+        </form>
+    </div>
 </x-layout>
